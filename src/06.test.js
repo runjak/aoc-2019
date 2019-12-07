271c13134802cf86ae7eee4012682fd810ef8390
Jakob Runge <sicarius@g4t3.de>
2019-12-07 09:33:24 +0100

    solve 06-1

--- src/06.test.js
+++ src/06.test.js
@@ -1,7 +1,7 @@
-import {} from "./06";
+import { getParents, toMap, indirectOrbits, countOrbits } from "./06";
 
 describe("06", () => {
-  const exampleInput = [
+  const example = [
     ["COM", "B"],
     ["B", "C"],
     ["C", "D"],
@@ -14,4 +14,50 @@ describe("06", () => {
     ["J", "K"],
     ["K", "L"]
   ];
+
+  describe("toMap()", () => {
+    it("should produce the desired example map", () => {
+      const expected = {
+        B: ["C", "G"],
+        C: ["D"],
+        COM: ["B"],
+        D: ["E", "I"],
+        E: ["F", "J"],
+        G: ["H"],
+        J: ["K"],
+        K: ["L"]
+      };
+
+      expect(toMap(example)).toEqual(expected);
+    });
+  });
+
+  describe("getParents()", () => {
+    it("should return the leafs from the example", () => {
+      expect(getParents(toMap(example))).toEqual(["COM"]);
+    });
+  });
+
+  describe("indirectOrbits()", () => {
+    it("should compute the indirect orbits for the example", () => {
+      const expected = {
+        B: ["C", "G", "D", "H", "E", "I", "F", "J", "K", "L"],
+        C: ["D", "E", "I", "F", "J", "K", "L"],
+        COM: ["B", "C", "G", "D", "H", "E", "I", "F", "J", "K", "L"],
+        D: ["E", "I", "F", "J", "K", "L"],
+        E: ["F", "J", "K", "L"],
+        G: ["H"],
+        J: ["K", "L"],
+        K: ["L"]
+      };
+
+      expect(indirectOrbits(toMap(example))).toEqual(expected);
+    });
+  });
+
+  describe("countOrbits()", () => {
+    it("should correctly count the example", () => {
+      expect(countOrbits(toMap(example))).toBe(42);
+    });
+  });
 });
--- src/06.ts
+++ src/06.ts
@@ -1,1 +1,64 @@
+import difference from "lodash/difference";
+import flatten from "lodash/flatten";
+import flatMap from "lodash/flatMap";
+import uniq from "lodash/uniq";
+import sum from "lodash/sum";
 import input from "./06.input.json";
+
+type OrbitList = Array<Array<string>>;
+type OrbitMap = { [source: string]: Array<string> };
+
+export const toMap = (orbitList: OrbitList): OrbitMap =>
+  orbitList.reduce(
+    (acc: OrbitMap, [parent, child]: Array<string>): OrbitMap => ({
+      ...acc,
+      [parent]: [...(acc[parent] || []), child]
+    }),
+    {}
+  );
+
+export const getParents = (orbitMap: OrbitMap): Array<string> => {
+  const parents = Object.keys(orbitMap);
+  const children = flatten(Object.values(orbitMap));
+
+  return difference(parents, children);
+};
+
+export const indirectOrbits = (orbitMap: OrbitMap): OrbitMap => {
+  let orbits = { ...orbitMap };
+  let changed = true;
+
+  while (changed) {
+    changed = false;
+
+    Object.keys(orbits).forEach((parent: string) => {
+      const children = orbits[parent];
+      const grandChildren = flatMap(
+        children,
+        (child: string): Array<string> => orbits[child] || []
+      );
+
+      const family = uniq([...children, ...grandChildren]);
+
+      if (family.length !== children.length) {
+        orbits[parent] = family;
+        changed = true;
+      }
+    });
+  }
+
+  return orbits;
+};
+
+export const countOrbits = (orbitMap: OrbitMap): number =>
+  sum(
+    Object.values(indirectOrbits(orbitMap)).map(
+      (labels: Array<string>): number => labels.length
+    )
+  );
+
+const task1 = (): number => countOrbits(toMap(input));
+
+export const solution = () => {
+  console.log(`06-1: ${task1()}`);
+};
