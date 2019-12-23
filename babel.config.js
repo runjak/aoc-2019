abfd45bccd9e1f287953507bca5ffccf17fa836e
Jakob Runge <sicarius@g4t3.de>
2019-12-24 00:23:26 +0100

    enable optional chaining

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,8 @@ module.exports = {
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript"
   ],
-  plugins: ["@babel/plugin-proposal-nullish-coalescing-operator"]
+  plugins: [
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-optional-chaining"
+  ]
 };
